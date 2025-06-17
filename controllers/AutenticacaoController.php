1595531c109e4ae52d19d0c935b2ccafa5367c51
Malik Ribeiro <166765009+MalikRibeiro@users.noreply.github.com>
2025-06-17 10:05:23 -0300

    Update AutenticacaoController.php

--- controllers/AutenticacaoController.php
+++ controllers/AutenticacaoController.php
@@ -5,14 +5,5 @@ class AutenticacaoController
     public function login()
     {
         include __DIR__ . '/../views/autenticacao/login.php';
-    }
-    public function cadastrar()
-    {
-        include __DIR__ . '/../views/autenticacao/cadastrar.php';
-    }
-    public function recuperar()
-    {
-        include __DIR__ . '/../views/autenticacao/recuperar.php';
-    }
-    
+    }    
 }
